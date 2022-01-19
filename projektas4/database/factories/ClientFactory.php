2df959c899cc880d508d95fcf1814d534ad36681
Mindaugas <qwerty93k@gmail.com>
2022-01-19 21:25:50 +0200

    faker sukurti random klientai

--- projektas4/database/factories/ClientFactory.php
+++ projektas4/database/factories/ClientFactory.php
@@ -13,8 +13,22 @@ class ClientFactory extends Factory
      */
     public function definition()
     {
+
+        //         ID
+        // name(string)
+        // surname(string)
+        // username(string)
+        // company_id(bigInteger) - skaicius
+        // image_url(string)
+
+        //Faker naudojam faker biblioteka
         return [
-            //
+            'name' => $this->faker->firstName(), // ims varda is faker bibliotekos
+            'surname' => $this->faker->lastName(),
+            'username' => $this->faker->userName(),
+            //'company_id' => rand(1, 250),
+            'company_id' => $this->faker->numberBetween(1, 250),
+            'image_url' => $this->faker->imageUrl(),
         ];
     }
 }
