71a37fdf0772b8909ebf64105deabcf4ce74c8f3
Mindaugas <qwerty93k@gmail.com>
2022-01-21 23:16:29 +0200

    summernote textarea

--- projektas4/resources/views/companies/edit.blade.php
+++ projektas4/resources/views/companies/edit.blade.php
@@ -6,6 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Company</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> {{-- jquery --}}
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">{{-- summer note --}}
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>{{-- summer note --}}
 </head>
 <body>
 <div class="container">
@@ -22,11 +25,19 @@
                 @endif
             @endforeach
         </select>
-        <input type="text" class="form-control" name="company_description" value={{$company->description}} placeholder="Company description">
+        {{--- <input type="text" class="form-control" name="company_description" value={{$company->description}} placeholder="Company description"> keiciame su textarea---}}
+        <textarea class="form-control" name="company_description" id="summernote">
+            {{$company->description}}
+        </textarea>
         @csrf
         <button type="submit" class="btn btn-primary">Save</button>
     </form>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script>
+    $(document).ready(function(){
+        $('#summernote').summernote();
+    });
+</script>
 </body>
 </html>
